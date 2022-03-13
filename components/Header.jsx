2f91280b2d3126f96f20f703e05eb4a16448c658
ssolis86 <ssolis8619@gmail.com>
2022-03-13 13:51:11 -0500

    add hamburger icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,6 +6,8 @@ import IconButton from "@mui/material/IconButton"
 import Home from "@mui/icons-material/Home"
 import MuiNextLink from "@components/MuiNextLink";
 import Navbar from './Navbar';
+import SideDrawer from "./SideDrawer";
+import HideOnScroll from "./HideOnScroll";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
@@ -20,27 +22,30 @@ export const navLinks =[
 const Header = () => {
   return (
     <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Container
-            maxWidth="lg"
-            sx={{ display: `flex`, justifyContent: `space-between` }}
-          >
-            <IconButton edge="start" aria-label="home">
-              <MuiNextLink activeClassName="active" href="/">
-                <Home
-                  sx={{
-                    color: (theme) => theme.palette.common.white,
-                  }}
-                  fontSize="large"
-                />
-              </MuiNextLink>
-            </IconButton>
+      <HideOnScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Container
+              maxWidth="lg"
+              sx={{ display: `flex`, justifyContent: `space-between` }}
+            >
+              <IconButton edge="start" aria-label="home">
+                <MuiNextLink activeClassName="active" href="/">
+                  <Home
+                    sx={{
+                      color: (theme) => theme.palette.common.white,
+                    }}
+                    fontSize="large"
+                  />
+                </MuiNextLink>
+              </IconButton>
 
-            <Navbar navLinks={navLinks} />
-          </Container>
-        </Toolbar>
-      </AppBar>
+              <Navbar navLinks={navLinks} />
+              <SideDrawer navLinks={navLinks} />
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
       <Offset />
     </>
   );
